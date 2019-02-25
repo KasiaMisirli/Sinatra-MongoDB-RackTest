22336310f6452f5fc76b371c54e763f73aa5e973
Ryan Bigg <me@ryanbigg.com>
2019-02-25 11:56:05 +1100

    Shared refactoring work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,10 @@ RSpec.configure do |config|
     Sinatra::Application
   end
 
+  config.before do
+    RatingQuestion.delete_all
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
